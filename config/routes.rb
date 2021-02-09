0f930db7f92542f9c6069f17291b43fbe33a5525
gonzalo <gologdlr@gmail.com>
2021-02-09 18:52:20 -0500

    add columns user id and image to Games

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   resources :games
-  get 'games/index'
-  get 'games/edit'
-  get 'games/show'
-  get 'games/new'
-  get 'games/_form'
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  root 'store#index'
+  root 'games#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_190019) do
+ActiveRecord::Schema.define(version: 2021_02_08_203303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2021_02_08_190019) do
     t.decimal "price", precision: 5, scale: 2, default: "0.0"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
